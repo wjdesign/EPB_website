23c81caa237da3354007b014d2cadcf2b09af062
wayne <hwiayne30192@yahoo.com.tw>
2020-05-17 19:26:45 +0800

    1.新增相關法規/資料下載 2.新增活動成果 3.新增跑馬燈

--- js/angular/controller/home.js
+++ js/angular/controller/home.js
@@ -17,12 +17,29 @@
             });
         };
 
+        // 輪播廣告
+        $scope.RunActive = function () {
+            $(function () {
+                console.log($('#activeSlide'));
+                $('#activeSlide').slick({
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    autoplay: true,
+                    autoplaySpeed: 3500,
+                    dots: false,
+                    prevArrow: $('.activePrev'),
+                    nextArrow: $('.activeNext')
+                });
+            });
+        };
+
         // Init
         function Init() {
             $scope.$parent.ShowLoading();
-            $scope.NewsList = NewsList;     // 取得data.js的假資料
+            $scope.Data = Data;     // 取得data.js的假資料
             console.log('home');
             $scope.RunCarousel();
+            $scope.RunActive();
             $scope.$parent.CloseLoading();
         }
         Init();
--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-var NewsList = {
+var Data = {
     "news": [
         {
             "url": "#",
@@ -54,6 +54,99 @@ var NewsList = {
         {
             "url": "#",
             "title": "環保志工為無給職，常年為社區環境清潔而努力，期盼權責機關能給予一點福利"
+        }
+    ],
+    "rule": [
+        {
+            "url": "#",
+            "title": "志願服務獎勵辦法"
+        },
+        {
+            "url": "#",
+            "title": "臺中市志願服務獎勵辦法"
+        },
+        {
+            "url": "#",
+            "title": "志願服務證及服務紀錄冊管理辦法"
+        },
+        {
+            "url": "#",
+            "title": "臺中市政府環境保護志願服務計畫"
+        },
+        {
+            "url": "#",
+            "title": "志願服務法"
+        }
+    ],
+    "download": [
+        {
+            "url": "#",
+            "title": "臺中市政府環境保護局志願服務計畫-108年6月12日修正",
+            "date": "2019/6/13"
+        },
+        {
+            "url": "#",
+            "title": "108年度臺中市環保志(義)工群英會競賽初賽-資源分類王考題出處",
+            "date": "2019/5/21"
+        },
+        {
+            "url": "#",
+            "title": "108年度臺中市環保志(義)工群英會競賽初賽-環保金頭腦、環境保衛戰題庫",
+            "date": "2019/5/21"
+        },
+        {
+            "url": "#",
+            "title": "108年度臺中市環保志(義)工群英會競賽初賽規則",
+            "date": "2019/5/13"
+        },
+        {
+            "url": "#",
+            "title": "108年度臺中市績優環保志工隊評鑑及獎勵實施計畫志工核銷(範例)資料",
+            "date": "2019/2/25"
+        }
+    ],
+    "active": [
+        {
+            "url": "#",
+            "img": "/EPB_website/images/active/01.jpg",
+            "title": "台灣燈會環保志工滿意度奪冠 志工當貴賓觀禮",
+            "actiondate": "109/2/22",
+            "update": "109/2/25"
+        },
+        {
+            "url": "#",
+            "img": "/EPB_website/images/active/02.jpg",
+            "title": "臺中清潔月，大家來拚厝",
+            "actiondate": "109/1/4",
+            "update": "109/1/6"
+        },
+        {
+            "url": "#",
+            "img": "/EPB_website/images/active/03.jpg",
+            "title": "108年績優環保志工隊表揚大會",
+            "actiondate": "108/11/3",
+            "update": "108/11/3"
+        },
+        {
+            "url": "#",
+            "img": "/EPB_website/images/active/04.jpg",
+            "title": "回收物品大變身！中市全國環保志工群英會勇奪2優勝",
+            "actiondate": "108/10/26",
+            "update": "108/10/27"
+        },
+        {
+            "url": "#",
+            "img": "/EPB_website/images/active/05.jpg",
+            "title": "我們準備好了！600名志工2020台灣燈會維護環境",
+            "actiondate": "108/10/20",
+            "update": "108/10/20"
+        },
+        {
+            "url": "#",
+            "img": "/EPB_website/images/active/06.jpg",
+            "title": "城市無名英雄！2020台灣燈會環保志工招募超過7成",
+            "actiondate": "108/10/6",
+            "update": "108/10/7"
         }
     ]
 };
